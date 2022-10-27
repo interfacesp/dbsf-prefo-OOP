001d47e67c192e28c8b9b36da5aac60fb3555fe6
Espoir Gahungere <espoir.gahungere@interface3.be>
2022-10-27 14:45:57 +0200

    Ingredient

--- practice/2-constructor/Ingredient.php
+++ practice/2-constructor/Ingredient.php
@@ -9,3 +9,20 @@
  * 
  * Ecrivez une méthode pour chaque propriété
  */
+
+class Ingredient { 
+    public string $nom; 
+    public int $quantity;
+
+    function __construct(string $newNom, int $quant)
+    {
+        $this->nom = $newNom;
+        $this->quantity= $quant;
+        echo "Coucou";
+    }
+}
+
+$piment = new Ingredient("Piment", 2);
+echo $piment->nom;
+echo $piment->quantity; 
+
