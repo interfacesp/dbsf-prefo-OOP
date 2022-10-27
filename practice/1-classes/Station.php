f7ee40652232636233c2c5943190de9f30353494
Espoir Gahungere <espoir.gahungere@interface3.be>
2022-10-27 13:43:51 +0200

    correction station

--- practice/1-classes/Station.php
+++ practice/1-classes/Station.php
@@ -2,7 +2,41 @@
 
 /**
  * Créer une classe Station (Transport en commun) avec comme propriétés 
- * nom, nombreIssues
+ * nom, nbExits
  * 
  * Ecrivez une méthode pour assigner des valeurs à chacune des propriétés 
  */
+
+class Station {
+    public string $st_name; 
+    public int $nbExits; 
+
+
+    function set_station_name(string $name): void{
+        $this->st_name = $name;
+    }
+
+    function set_nb_exits(int $numberExits): void {
+        $this->nbExits = $numberExits;
+    }
+
+    function pretty_print():void {
+        echo "<h1>La station $this->st_name a $this->nbExits sorties </h1>";
+    }
+
+    function get_nbExits(): int {
+        return $this->nbExits;
+    }
+}
+
+$simonis = new Station();
+$simonis->set_station_name("Simonis");
+$simonis->set_nb_exits(3);
+$simonis->pretty_print();
+
+
+$arts_loi = new Station();
+$arts_loi->set_station_name("Arts-Loi");
+$arts_loi->set_nb_exits(2);
+$arts_loi->pretty_print();
+
